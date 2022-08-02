3d970fcb4d9c4f7dd2b256afaf31d24742ddd77a
PeterArce96 <parce0496@gmail.com>
2022-08-02 15:41:36 -0500

    inicio de angular routing, 3 componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComprasComponent } from './compras/compras.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { VentasComponent } from './ventas/ventas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'ventas', component: VentasComponent},
+  {path: 'compras', component: ComprasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { ComprasComponent } from './compras/compras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    VentasComponent,
+    ComprasComponent
   ],
   imports: [
     BrowserModule,
